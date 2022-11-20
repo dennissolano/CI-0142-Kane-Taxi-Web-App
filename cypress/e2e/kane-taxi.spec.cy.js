0afb9060c6cea889f84f7ddb3bc30a5d01bb307d
Edu F <eduferbaldi@gmail.com>
2022-11-20 14:44:54 -0600

    Added edit driver test and export report test

--- cypress/e2e/kane-taxi.spec.cy.js
+++ cypress/e2e/kane-taxi.spec.cy.js
@@ -1,11 +1,11 @@
 import HomePage from '../pages/HomePage';
 
 describe('Kane Taxi Tests Suite', () => {
-  it.skip('testLogin', () => {
+  it('testLogin', () => {
     cy.login();
   });
 
-  it.skip('testLogout', () => {
+  it('testLogout', () => {
     cy.login();
     cy.logout();
   });
@@ -49,4 +49,43 @@ describe('Kane Taxi Tests Suite', () => {
 
     cy.logout();
   });
+
+  it('testEditTaxiDriver', () => {
+    cy.login();
+    cy.fixture('testTaxiDriverDetails').then((taxiDriverDetails) => {
+      HomePage.openEditTaxiDriverByName(taxiDriverDetails.name);
+    });
+
+    cy.wait(3000)
+    cy.xpath(
+      `//*[@id="idCheckbox"]`
+    ).click();
+
+    cy.xpath(
+        `//html/body/app-root/app-detalles-taxista/form/div/div[1]/div[1]/div[3]/button`
+    ).click();
+
+    cy.wait(3000)
+    cy.xpath(`//*[@id="toast-container"]`).should(
+        'not.be.empty'
+    );
+  });
+
+  it('testExportTaxiDriverReport', () => {
+    cy.login();
+
+    cy.xpath(
+      `//html/body/app-root/app-home/app-header/ul/li[4]/a`
+    ).click();
+
+    cy.xpath(
+      `//html/body/app-root/app-reports/div/div[1]/div[1]/button`
+    ).click();
+    
+    cy.xpath(
+      `//html/body/app-root/app-reports/div/div[2]/div/div[1]/button`
+    ).click();
+
+  });
+
 });
--- cypress/pages/HomePage.js
+++ cypress/pages/HomePage.js
@@ -4,5 +4,11 @@ class HomePage {
       `//li[contains(text(), '${name}')]/ancestor::div[contains(@class, 'card')][2]//div[contains(text(), 'Detalles')]`
     ).click();
   }
+
+  openEditTaxiDriverByName(name) {
+    cy.xpath(
+      `//li[contains(text(), '${name}')]/ancestor::div[contains(@class, 'card')][2]//div[contains(text(), 'Editar')]`
+    ).click();
+  }
 }
 module.exports = new HomePage();
