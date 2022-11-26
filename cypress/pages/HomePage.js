b998aaefb03c29581629b9ab61a9e18d5a77b6b8
Dennis Alberto Solano Padilla <DennisAlberto.Solano@microfocus.com>
2022-11-26 11:52:00 -0600

    Added search user by first name utils

--- cypress/pages/HomePage.js
+++ cypress/pages/HomePage.js
@@ -1,4 +1,9 @@
 class HomePage {
+  elements = {
+    searchBar: () => cy.xpath("//input[@name='driverName']"),
+    searchButton: () => cy.xpath("//button[text()='Buscar']")
+  };
+
   openTaxiDriverDetailsByName(name) {
     cy.xpath(
       `//li[contains(text(), '${name}')]/ancestor::div[contains(@class, 'card')][2]//div[contains(text(), 'Detalles')]`
@@ -9,6 +14,12 @@ class HomePage {
     cy.xpath(
       `//li[contains(text(), '${name}')]/ancestor::div[contains(@class, 'card')][2]//div[contains(text(), 'Editar')]`
     ).click();
+  }
+
+  searchUserByFirstName(userName) {
+    cy.log(`Searching user ${userName}`);
+    this.elements.searchBar().clear().type(userName);
+    this.elements.searchButton().click();
   }
 }
 module.exports = new HomePage();
